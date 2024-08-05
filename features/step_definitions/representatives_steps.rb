2e646bf671211d4bc6e27582bb84c9366b6db9f5
Josh Zheng <joshzheng99@berkeley.edu>
2024-08-05 21:05:14 +0000

    added cucumber step defnitions

--- features/step_definitions/representatives_steps.rb
+++ features/step_definitions/representatives_steps.rb
@@ -40,3 +40,19 @@ end
 Then('I should see the representative\'s photo') do
   expect(page).to have_selector("img[src='#{@representative.photo_url}']")
 end
+
+When(/^I click on state (.+)$/) do |state|
+  visit state_map_path(state)
+end
+
+When(/^I click on county (.+)$/) do |county|
+  visit search_representatives_path(county)
+end
+
+Then(/^(?:|I )should see "([^"]*)"$/) do |text|
+  if page.respond_to? :should
+      page.should have_content(text)
+  else
+      assert page.has_content?(text)
+  end
+end
