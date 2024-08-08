6a46ae7197d0515934bbe6c19f529b543affb984
oyrange <lojewski@berkeley.edu>
2024-08-08 10:55:42 -0700

    Test basic functionality of news_item

--- spec/models/news_item_spec.rb
+++ spec/models/news_item_spec.rb
@@ -5,10 +5,28 @@ require 'spec_helper'
 require 'google/apis/civicinfo_v2'
 
 describe NewsItem do
-  let(:news_info) { {representative_id:1, title:"Title", link:"website.com",
+  let(:news_info) { {representative_id:1, title:"The Title", link:"website.com",
                      issue:"Free Speech"} }
-  it 'creates a news item' do
-    news1 = described_class.create(news_info)
-    expect(news1.issue).should_not be_nil
+  let(:news_article1) { described_class.create(news_info) }
+
+  context 'creates a news item' do
+    it 'that is not nil' do
+      expect(news_article1).should_not be_nil
+    end
+
+    context 'with the correct' do
+      it 'representative ID' do
+        expect(news_article1.representative_id).to eq(news_info[:representative_id])
+      end
+      it 'title' do
+        expect(news_article1.title).to eq(news_info[:title])
+      end
+      it 'link' do
+        expect(news_article1.link).to eq(news_info[:link])
+      end
+      it 'issue' do
+        expect(news_article1.issue).to eq(news_info[:issue])
+      end
+    end
   end
 end
