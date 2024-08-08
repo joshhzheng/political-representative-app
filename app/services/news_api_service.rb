3c9b9f31c132791cece56cffeb7fa5ddf668cbc5
Faith <f.dennis1798@gmail.com>
2024-08-08 16:54:55 -0700

    added documentation

--- app/services/news_api_service.rb
+++ app/services/news_api_service.rb
@@ -1,3 +1,7 @@
+# Service object used to implement business needs from the News API. 
+# initialize creates the service object with a call to new.
+# fetch_top_articles queries the representative and issue and returns 1 page of up to 5 results. The language is expected to be in English and the origin from the US. 
+
 class NewsAPIService
   def initialize(api_key = Rails.application.credentials[:NEWS_API_KEY])
     @news_api = News.new(api_key)
@@ -5,6 +9,6 @@ class NewsAPIService
 
   def fetch_top_articles(representative_name, issue)
     query = "#{representative_name} #{issue}"
-    @news_api.get_top_headlines(q: query, language: 'en', country: 'us')
+    @news_api.get_top_headlines(q: query, pageSize: 5, page: 1, language: 'en', country: 'us')
   end
 end
