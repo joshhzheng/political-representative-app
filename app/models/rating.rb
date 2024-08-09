ba4a08182861eb47956374af77410c188caddb5e
oyrange <lojewski@berkeley.edu>
2024-08-09 12:44:19 -0700

    Fix linter issues, minor rating spec and rating.rb refactoring

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,7 +2,7 @@
 
 class Rating < ApplicationRecord
   belongs_to :news_item
-  RATINGS =* (1..5).freeze
+  RATINGS = (1..5).freeze
 
   validates :score, presence: true, inclusion: { in: RATINGS }
   validates :comment, presence: false
